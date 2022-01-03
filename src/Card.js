1bf76b2cef2dd8e306e42b23da4d139731642d01
Benjamin GUILLAUME <benjamin.guillaume@syspertec.com>
2022-01-03 21:32:40 +0100

    📦 NEW: Mise en place du projet. Premier composant Card

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Card.css";
+
+const HIDDEN_SYMBOL = "❓";
+
+// const Card = () => <div className="card" />
+
+const Card = () => <div>{HIDDEN_SYMBOL}</div>;
+
+export default Card;
